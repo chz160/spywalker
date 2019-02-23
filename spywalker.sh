77637fe0a1b1784d56a2509f852024c81ac9766f
chz160 <noahporch@gmail.com>
2019-02-23 14:16:35 -0600

    Working Directory

--- spywalker.sh
+++ spywalker.sh
@@ -4,9 +4,12 @@ workingDir=$2
 if test -f spywalker.conf ; then
 	. spywalker.conf
 fi
-if [ homeBaseSsid == "" ]; then
+if [ "$homeBaseSsid" == "" ]; then
 	echo "homeBaseSsid has no value. This must be set as a parameter or in spywalker.conf." 1>&2
 	exit 1
+fi
+if [ "$workingDir" == "" ]; then
+	workingDir="$PWD"
 fi
 session=walk
 window=${session}:0
@@ -14,9 +17,7 @@ pane1=${window}.0
 pane2=${window}.1
 pane3=${window}.2
 pane4=${window}.3
-if [ workingDir != "" ]; then
-	cd $workingDir
-fi
+cd $workingDir
 tmux new -s walk -d \; \
 	split-window -v \; \
 	select-pane -t 0 \; \
