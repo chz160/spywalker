75a772ee7062eb5fef9c63f9c28c13bb82cf01e8
chz160 <noahporch@gmail.com>
2019-03-02 17:17:55 -0600

    Changed hcx dump script

--- capture_hcxdump.sh
+++ capture_hcxdump.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 . /etc/default/spywalker.conf
 #wlanInterfaceName=${hcxdumpInterface}
-read monHcxInterface <<< $(sudo ifconfig | awk 'match($1,"${hcxdumpInterface}mon") {print substr($1,RSTART,RLENGTH)}')
+searchMonInterface="${hcxdumpInterface}mon"
+echo "Searching for $searchMonInterface, if unavailable, setting it up..."
+read monHcxInterface <<< $(sudo ifconfig | awk 'match($1,"$searchMonInterface") {print substr($1,RSTART,RLENGTH)}')
 if [ -z "$monHcxInterface" ]
 then
 	echo "Killing troublesome processes..."
 	sudo airmon-ng check kill
-        echo "Creating monitor interface on ${hcxdumpInterface}..."
-        sudo airmon-ng start ${hcxdumpInterface}
-	read monHcxInterface <<< $(ifconfig | awk 'match($1,"${hcxdumpInterface}mon") {print substr($1,RSTART,RLENGTH)}')
+        echo "Creating monitor interface on $hcxdumpInterface..."
+        sudo airmon-ng start $hcxdumpInterface
+	read monHcxInterface <<< $(ifconfig | awk 'match($1,"$searchMonInterface") {print substr($1,RSTART,RLENGTH)}')
 else
         echo "Monitor interface already exists as: $monHcxInterface"
 fi
