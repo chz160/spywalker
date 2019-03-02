2239c3c747b00f36ae1afcf5a39941801c3818f5
chz160 <noahporch@gmail.com>
2019-03-02 17:05:34 -0600

    Changes to kismet script

--- capture_hcxdump.sh
+++ capture_hcxdump.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 . /etc/default/spywalker.conf
 #wlanInterfaceName=${hcxdumpInterface}
-read monInterfaceName <<< $(sudo ifconfig | awk 'match($1,"${hcxdumpInterface}mon") {print substr($1,RSTART,RLENGTH)}')
-if [ -z "$monInterfaceName" ]
+read monHcxInterface <<< $(sudo ifconfig | awk 'match($1,"${hcxdumpInterface}mon") {print substr($1,RSTART,RLENGTH)}')
+if [ -z "$monHcxInterface" ]
 then
 	echo "Killing troublesome processes..."
 	sudo airmon-ng check kill
         echo "Creating monitor interface on ${hcxdumpInterface}..."
         sudo airmon-ng start ${hcxdumpInterface}
-	read monInterfaceName <<< $(ifconfig | awk 'match($1,"${hcxdumpInterface}mon") {print substr($1,RSTART,RLENGTH)}')
+	read monHcxInterface <<< $(ifconfig | awk 'match($1,"${hcxdumpInterface}mon") {print substr($1,RSTART,RLENGTH)}')
 else
-        echo "Monitor interface already exists as: $monInterfaceName"
+        echo "Monitor interface already exists as: $monHcxInterface"
 fi
 # Get Wifis that are in range.
-#sudo hcxdumptool -i $monInterfaceName -o capture.pcapng --enable_status=15 --use_gpsd
-sudo hcxdumptool -i $monInterfaceName -o capture.pcapng --enable_status=15
+#sudo hcxdumptool -i $monHcxInterface -o capture.pcapng --enable_status=15 --use_gpsd
+sudo hcxdumptool -i $monHcxInterface -o capture.pcapng --enable_status=15
--- capture_kismet.sh
+++ capture_kismet.sh
@@ -3,16 +3,16 @@
 #sudo iw phy `iw dev wlan0 info | gawk '/wiphy/ {printf "phy" $2}'` interface add ${kismetInterface}mon type monitor
 #sudo ifconfig ${kismetInterface}mon up
 
-read monInterfaceName <<< $(sudo ifconfig | awk 'match($1,"${kismetInterface}mon") {print substr($1,RSTART,RLENGTH)}')
-if [ -z "$monInterfaceName" ]
-then
-    echo "Creating monitor interface on ${kismetInterface}..."
-    sudo airmon-ng start ${kismetInterface}
-	read monInterfaceName <<< $(ifconfig | awk 'match($1,"${kismetInterface}mon") {print substr($1,RSTART,RLENGTH)}')
-else
-        echo "Monitor interface already exists as: $monInterfaceName"
-fi
+# read monKismetInterface <<< $(sudo ifconfig | awk 'match($1,"${kismetInterface}mon") {print substr($1,RSTART,RLENGTH)}')
+# if [ -z "$monKismetInterface" ]
+# then
+#     echo "Creating monitor interface on ${kismetInterface}..."
+#     sudo airmon-ng start ${kismetInterface}
+# 	read monKismetInterface <<< $(ifconfig | awk 'match($1,"${kismetInterface}mon") {print substr($1,RSTART,RLENGTH)}')
+# else
+#         echo "Monitor interface already exists as: $monKismetInterface"
+# fi
 
-sudo airmon-ng start ${monInterfaceName}
-sudo kismet -c ${monInterfaceName}
+#sudo airmon-ng start ${monKismetInterface}
+sudo kismet -c ${kismetInterface}
   
