f54b13dc40aa62e8bb44524ded0492679bf7d777
chz160 <noahporch@gmail.com>
2019-03-03 14:43:06 -0600

    Restructed online status check

--- online_status.sh
+++ online_status.sh
@@ -8,12 +8,13 @@ do
         ifdown --force $onboardInterface
         ifup $onboardInterface
         sleep 5
-        wget -q --tries=10 --timeout=20 --spider http://google.com
-        if [[ $? -eq 0 ]]; then
-            echo "Conneted to homebase."
-        else
-            echo "Disconneted from homebase."
-        fi
+        ssid=$(iw $onboardInterface link | grep ssid)
+    fi
+    wget -q --tries=10 --timeout=20 --spider http://google.com
+    if [ $? -eq 0 ] && [ "$ssid" == "$homeBaseSsid" ]; then
+        echo "Connected to homebase wifi."
+    else
+        echo "Disconnected from homebase wifi."
     fi
     sleep 10
 done
